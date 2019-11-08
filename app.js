bf18ba9b0dc26ddffa6091a2a96be0c475f3132c
thangcx-0794 <chu.xuan.thang@sun-asterisk.com>
2019-11-08 16:56:04 +0700

    master

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ var sudokuApp = new Vue({
         answerImage: "",
         isGameStarted: false,
         showAnswer: false,
-        isNewGame: true,
-    },
-
-    mounted() {
-        // Preload image
-        new Image().src = 'fail.gif';
-        new Image().src = 'success.gif';
     },
 
     methods: {
@@ -34,7 +27,6 @@ var sudokuApp = new Vue({
 
             this.sudokuMatrix = defaultSudokuMatrix;
             this.isGameStarted = true;
-            this.isNewGame = false;
         },
 
         evaluateGame() {
@@ -45,24 +37,17 @@ var sudokuApp = new Vue({
                 this.answerImage = "success.gif";
                 this.showAnswer = true;
                 this.isGameStarted = false;
-                this.isNewGame = false;
 
                 setTimeout(() => {
                     this.showAnswer = false;
-                    this.isGameStarted = true;
-                    this.isNewGame = false;
                 }, 2000);
             }
             else {
                 this.answerImage = "fail.gif";
                 this.showAnswer = true;
-                this.isGameStarted = false;
-                this.isNewGame = false;
 
                 setTimeout(() => {
                     this.showAnswer = false;
-                    this.isGameStarted = true;
-                    this.isNewGame = false;
                 }, 2000);
             }
         },
@@ -70,7 +55,6 @@ var sudokuApp = new Vue({
         restart() {
             this.isGameStarted = false;
             this.showAnswer = false;
-            this.isNewGame = true;
         },
 
         formatCell(row, cell) {
